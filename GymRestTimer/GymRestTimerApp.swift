d5886e3738bfe395d6f4339225e52f0f176ab582
Kotinos <aaronchanlin@gmail.com>
2025-06-27 10:10:33 -0400

    Initial Commit

---
+++ GymRestTimer/GymRestTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymRestTimerApp.swift
+//  GymRestTimer
+//
+//  Created by Aaron Lin on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct GymRestTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymRestTimerUITests/GymRestTimerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  GymRestTimerUITestsLaunchTests.swift
+//  GymRestTimerUITests
+//
+//  Created by Aaron Lin on 6/27/25.
+//
+
+import XCTest
+
+final class GymRestTimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
